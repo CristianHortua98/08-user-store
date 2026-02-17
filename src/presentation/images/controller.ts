a97d9102a9779e71df64c859c7b327ab053ab584
CAS <alejo.hortua3@gmail.com>
2026-02-16 20:20:58 -0500

    Images

---
+++ src/presentation/images/controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import fs from 'fs';
+import path from "path";
+
+export class ImageController {
+
+    constructor(){}
+
+
+    getImage = (req: Request, res: Response) => {
+
+        const { type = '', img = '' } = req.params;
+
+        console.log(img)
+
+        const imagePath = path.resolve(__dirname, `../../../uploads/${type}/${img}`);
+
+        if(!fs.existsSync(imagePath)){
+            return res.status(404).send('Image not found');
+        }
+
+        res.sendFile(imagePath);
+
+    }
+
+}
